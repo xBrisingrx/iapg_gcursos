7fd50c3e80af2b9b6c9612beccce9ae6604989c7
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-05 13:56:55 -0300

    add method to display errors messages in forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def inline_error_for(field, form_obj)
+    html = []
+    if form_obj.errors[field].any?
+      html << form_obj.errors[field].map do |msg|
+        tag.div(msg, class: "text-danger")
+      end
+    end
+    html.join.html_safe
+  end
 end
