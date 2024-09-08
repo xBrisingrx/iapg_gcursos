e35c81fde7c603dbbbc51f849539378b3405ef8a
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-08 18:02:17 -0300

    change(form error helper): I can add a css classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def inline_error_for(field, form_obj)
+  def inline_error_for(field, form_obj, classes = "")
     html = []
     if form_obj.errors[field].any?
       html << form_obj.errors[field].map do |msg|
-        tag.div(msg, class: "text-danger")
+        tag.div(msg, class: "text-danger #{classes}")
       end
     end
     html.join.html_safe
