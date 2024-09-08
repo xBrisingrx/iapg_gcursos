57c7dda6d0f1eef4f15b7321223b486096427b91
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-08 18:01:11 -0300

    set default language

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,8 @@ module IapgGcursos
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.time_zone = "America/Argentina/Buenos_Aires"
+    config.i18n.default_locale = :es
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
