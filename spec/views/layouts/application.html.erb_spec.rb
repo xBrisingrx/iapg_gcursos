1c713003f88e2b86ff03d3a5037b6bfd81b02249
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-27 12:59:55 -0300

    add spec to layous template

---
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'layouts/application', type: :view do
+  it 'render the layout' do
+    # allow nos permite agregar los expect de los llamados de las vistas
+    # and_call_original nos permite hacer las llamadas de renderizado
+    allow(view).to receive(:render).and_call_original
+    render
+
+    expect(view).to have_received(:render).with('shared/navbar')
+  end
+end
