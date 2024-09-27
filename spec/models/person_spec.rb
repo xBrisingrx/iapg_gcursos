d5a7883bc03518917648037ca6d8e57f7cea9794
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-27 13:02:04 -0300

    change person spec: add validation spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:person) }
+
+  describe 'validations' do
+    it { is_expected.to be_valid }
+    it { is_expected.to belong_to(:province).optional }
+    it { is_expected.to belong_to(:city).optional }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:name) } 
+    it { is_expected.to validate_presence_of(:cuil) } 
+    it { is_expected.to validate_presence_of(:birthdate) } 
+    it { is_expected.to validate_presence_of(:phone) } 
+    it { is_expected.to validate_presence_of(:celphone) } 
+    it { is_expected.to validate_presence_of(:email) } 
+    it { is_expected.to validate_presence_of(:direction) } 
+    # esta validacion me da error en la asociacion
+    # it { is_expected.to validate_uniqueness_of(:cuil) }
+  end
 end
