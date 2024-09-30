eb6b8cc1a40d26319becf881a7c319edecf9af9e
Mauro Almiron <mdavid.almiron@gmail.com>
2024-09-30 15:03:11 -0300

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  resources :company_areas, except: [:destroy] do
-    get 'modal_disable', on: :member
-    put 'disable', on: :member
+  resources :company_areas, except: [ :destroy ] do
+    get "modal_disable", on: :member
+    put "disable", on: :member
   end
-  resources :company_categories, except: [:destroy] do
-    get 'modal_disable', on: :member
-    put 'disable', on: :member
+  resources :company_categories, except: [ :destroy ] do
+    get "modal_disable", on: :member
+    put "disable", on: :member
   end
-  resources :people
+  resources :people, except: [ :destroy ] do
+    get "modal_disable", on: :member
+    put "disable", on: :member
+  end
   get "main/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
@@ -20,5 +19,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "main#index"
+  root "people#index"
 end
