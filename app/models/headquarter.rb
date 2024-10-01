facba1aaebd0303c1fea2d0f2607ab860be16c8f
Mauro Almiron <mdavid.almiron@gmail.com>
2024-10-01 12:37:01 -0300

    change headquarters
    set relations in model & set ransack config

--- app/models/headquarter.rb
+++ app/models/headquarter.rb
@@ -1,11 +1,26 @@
 class Headquarter < ApplicationRecord
+  belongs_to :city
+  belongs_to :province
+  belongs_to :sectional
+
   validates :name,
     presence: true,
     uniqueness: { case_sensitive: false }
 
+  before_validation :set_province
+
   scope :actives, -> { where(active: true) }
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "id", "name", "description" ]
+    [ "id", "name", "description", "province_id", "city_id", "sectional_id", "can_make_psychometric" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "sectional", "city", "province" ]
+  end
+
+  private
+  def set_province
+    self.province = self.city.province if !self.city_id.blank?
   end
 end
