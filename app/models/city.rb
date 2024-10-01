cf23d9bab6c16bb9afd9e7b6eef5f430b010c7c4
Mauro Almiron <mdavid.almiron@gmail.com>
2024-10-01 12:04:52 -0300

    set config to use ransack

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,4 +4,8 @@ class City < ApplicationRecord
   validates :name, 
     presence: true,
     uniqueness: { case_sensitive: false, scope: :province_id }
+  
+  def self.ransackable_attributes(auth_object = nil)
+    [ "id", "name" ]
+  end
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -3,4 +3,8 @@ class Province < ApplicationRecord
   validates :name, 
     presence: true,
     uniqueness: { case_sensitive: false }
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "id", "name" ]
+  end
 end
