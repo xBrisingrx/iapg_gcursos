de7b50eb8eb792a4d5bb452a4b0a80fd6b321734
Mauro Almiron <mdavid.almiron@gmail.com>
2024-10-25 15:05:55 -0300

    avance

--- app/controllers/course_units_controller.rb
+++ app/controllers/course_units_controller.rb
@@ -5,6 +5,25 @@ class CourseUnitsController < ApplicationController
   end
 
   def create
+    course = Course.find(params[:course_id])
+    course_unit = course.course_units.new(course_unit_params)
+    day = course.course_type.course_type_units.find_by(unit_id: course_unit.unit_id, shift: course_unit.shift ).day
+    course_unit.day = day
+
+    respond_to do |format|
+      if course_unit.save
+        format.turbo_stream {
+          render turbo_stream: [
+            turbo_stream.replace("toasts",
+              partial: "shared/toasts",
+              locals: { message: "Módulo agregado", status_class: "primary" })
+          ]
+        }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @iva_condition.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
@@ -12,7 +31,7 @@ class CourseUnitsController < ApplicationController
       @course_unit = CourseUnit.find(params[:id])
     end
 
-    def course_params
-      params.require(:course).permit(:from_date, :year_number, :general_number, :is_company, :course_type_id)
+    def course_unit_params
+      params.require(:course_unit).permit(:course_id, :unit_id, :instructor_id ,:shift, :start_hour, :end_hour)
     end
 end
--- app/models/course_person.rb
+++ app/models/course_person.rb
@@ -41,7 +41,7 @@ class CoursePerson < ApplicationRecord
     # seteamos la hora si es que el modulo va por turnos
     # debemos tener en cuenta que no se pise con turnos de otros ni con un turno q tenga esta persona
     # ya que el mismo dia puede tener practica y psicometrico
-    turn_available = Turn.where(course_id: course_id, unit_id: unit_id, available: true)
+    turn_available = Turn.where(course_id: course_id, unit_id: unit_id, available: true).order(:hour)
     turn_hour = nil
     turn_available.each do |turn|
       if self.person_is_available(shift_time, date, turn.hour)
