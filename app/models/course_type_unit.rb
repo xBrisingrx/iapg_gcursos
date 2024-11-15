dd4bf8e5e4e7e5288ec5f97c8734f58ab0360d7c
Mauro Almiron <mdavid.almiron@gmail.com>
2024-11-15 14:54:27 -0300

    add validations

--- app/models/course_type_unit.rb
+++ app/models/course_type_unit.rb
@@ -2,6 +2,13 @@ class CourseTypeUnit < ApplicationRecord
   belongs_to :course_type
   belongs_to :unit
 
+  validates :shift_time, 
+    presence: { messsage: "Debe aclarar el tiempo de cada turno" }, 
+    if: :unit_is_by_turn?
+  validates :start_hour, :end_hour,presence: true
+  validate :start_hour_less_than_end_hour
+
+
   def schedule
     "De #{self.start_hour&.strftime("%k:%M")} a #{self.end_hour&.strftime("%k:%M")}"
   end
@@ -10,5 +17,18 @@ class CourseTypeUnit < ApplicationRecord
     return "--" if !self.is_by_turn
     diff_in_minutes = (self.end_hour - self.start_hour) / 60
     (diff_in_minutes / self.shift_time).to_i
+  end
+
+  private
+  def start_hour_less_than_end_hour
+    return if (self.start_hour.nil? || self.end_hour.nil?)
+    if self.start_hour >= self.end_hour
+      errors.add :start_hour, "Hora inicio debe ser menor a hora fin"
+      errors.add :end_hour, "Hora fin debe ser mayor a hora inicio"
+    end
+  end
+
+  def unit_is_by_turn?
+    self.is_by_turn
   end
 end
